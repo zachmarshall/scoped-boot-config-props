5ae98e0a57a60a7c3e9fe09849f6ff558582c4c1
Vladislav Iofe <viofe@custis.ru>
2023-07-19 13:05:29 +0500

    boot-2.3.12
    Commit time branch was boot-2.3.

--- src/main/java/org/example/ConfigPropertiesScopeConfig.java
+++ src/main/java/org/example/ConfigPropertiesScopeConfig.java
@@ -1,13 +1,12 @@
 package org.example;
 
-import org.springframework.boot.context.properties.ConfigurationBeanFactoryMetadata;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigPropertiesScopeConfig {
     @Bean
-    ConfigPropertiesScopePostProcessor configPropertiesScopePostProcessor(ConfigurationBeanFactoryMetadata metaData) {
-        return new ConfigPropertiesScopePostProcessor(metaData);
+    ConfigPropertiesScopePostProcessor configPropertiesScopePostProcessor() {
+        return new ConfigPropertiesScopePostProcessor();
     }
 }
--- src/main/java/org/example/ConfigPropertiesScopePostProcessor.java
+++ src/main/java/org/example/ConfigPropertiesScopePostProcessor.java
@@ -2,24 +2,34 @@ package org.example;
 
 import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.boot.context.properties.ConfigurationBeanFactoryMetadata;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.annotation.MergedAnnotation;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Detects {@link ConfigurationProperties} beans in the Spring context and
  * creates a scoped proxy for them.
+ *
+ * Part of the code used from <a href="https://github.com/spring-projects/spring-boot/blob/main/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/context/properties/ConfigurationPropertiesBean.java">ConfigurationPropertiesBean</a>
  */
 public class ConfigPropertiesScopePostProcessor implements BeanFactoryPostProcessor {
-    ConfigurationBeanFactoryMetadata metaData;
 
-    public ConfigPropertiesScopePostProcessor(ConfigurationBeanFactoryMetadata metaData) {
-        this.metaData = metaData;
+    public ConfigPropertiesScopePostProcessor() {
     }
 
     /**
@@ -27,26 +37,28 @@ public class ConfigPropertiesScopePostProcessor implements BeanFactoryPostProces
      * {@link ConfigurationProperties} bean and also not registered to be skipped by
      * this processor.
      *
-     * @param beanClass
-     *            The class of the bean.
+     * @param beanFactory
+     *            The bean factory.
      * @param beanName
      *            The name of the bean. This is used to find annotation metadata in
      *            the context.
      * @return true if the bean is a {@link ConfigurationProperties} bean and not
      *         skipped.
      */
-    private boolean isConfigPropBeanAndProxyable(Class<?> beanClass, String beanName) {
-        boolean isConfigProps = false;
-        ConfigurationProperties annotation = AnnotationUtils.findAnnotation(beanClass, ConfigurationProperties.class);
-        if (annotation != null) {
-            isConfigProps = true;
-        } else if (this.metaData != null) {
-            annotation = this.metaData.findFactoryAnnotation(beanName, ConfigurationProperties.class);
-            if (annotation != null) {
-                isConfigProps = true;
+    private boolean isConfigPropBeanAndProxyable(ConfigurableListableBeanFactory beanFactory, String beanName) {
+        try {
+            if (beanFactory.getBeanDefinition(beanName).isAbstract()) {
+                return false;
             }
+            if (beanFactory.findAnnotationOnBean(beanName, ConfigurationProperties.class) != null) {
+                return true;
+            }
+            Method factoryMethod = findFactoryMethod(beanFactory, beanName);
+            return findMergedAnnotation(factoryMethod, ConfigurationProperties.class).isPresent();
         }
-        return isConfigProps;
+        catch (NoSuchBeanDefinitionException ex) {
+            return false;
+        }
     }
 
     /**
@@ -68,31 +80,70 @@ public class ConfigPropertiesScopePostProcessor implements BeanFactoryPostProces
         if (!(beanFactory instanceof BeanDefinitionRegistry)) {
             throw new IllegalArgumentException("Can only process BeanDefinitionRegistry instances");
         }
+        boolean oldAllowBeanDefinitionOverriding = false;
+        if (beanFactory instanceof DefaultListableBeanFactory){
+            oldAllowBeanDefinitionOverriding = ((DefaultListableBeanFactory)beanFactory).isAllowBeanDefinitionOverriding();
+            ((DefaultListableBeanFactory)beanFactory).setAllowBeanDefinitionOverriding(true);
+        }
 
         // Register dummy custom scope. Real code is more interesting.
         beanFactory.registerScope(CustomScope.NAME, new CustomScope());
 
         BeanDefinitionRegistry registry = (BeanDefinitionRegistry) beanFactory;
         for (String beanName : beanFactory.getBeanDefinitionNames()) {
-            Class<?> clazz = beanFactory.getType(beanName);
-            if (isConfigPropBeanAndProxyable(clazz, beanName)) {
+            if (isConfigPropBeanAndProxyable(beanFactory, beanName)) {
                 BeanDefinition bd = beanFactory.getBeanDefinition(beanName);
                 BeanDefinitionHolder bdh = new BeanDefinitionHolder(bd, beanName);
                 bd.setScope(CustomScope.NAME);
-                bdh = ScopedProxyUtils.createScopedProxy(bdh, registry, true);
-                registry.registerBeanDefinition(beanName, bdh.getBeanDefinition());
+                BeanDefinitionHolder scopedBdh = ScopedProxyUtils.createScopedProxy(bdh, registry, true);
+                registry.registerBeanDefinition(beanName, scopedBdh.getBeanDefinition());
             }
         }
-        // We just added some new bean definitions to the registry (e.g., named
-        // scopedTarget.<orignalName>). They are used when
-        // org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessor.postProcessBeforeInitialization(Object,String)
-        // post processes the new aop proxy target bean instance. However, without the
-        // line below, the metaData used by ConfigurationPropertiesBindingPostProcessor
-        // won't have the scopedTarget bean name in its list and therefore, if the
-        // @ConfigurationProperties annotation was on a factory method, then
-        // ConfigurationPropertiesBindingPostProcessor wouldn't find it. This makes sure
-        // that these additional new bean method names are found after they have been
-        // added to the bean definition registry.
-        metaData.postProcessBeanFactory(beanFactory);
+
+        if (beanFactory instanceof DefaultListableBeanFactory){
+            ((DefaultListableBeanFactory)beanFactory).setAllowBeanDefinitionOverriding(oldAllowBeanDefinitionOverriding);
+        }
     }
+
+    private static <A extends Annotation> MergedAnnotation<A> findMergedAnnotation(AnnotatedElement element,
+                                                                                   Class<A> annotationType) {
+        return (element != null) ? MergedAnnotations.from(element, MergedAnnotations.SearchStrategy.TYPE_HIERARCHY).get(annotationType)
+                : MergedAnnotation.missing();
+    }
+
+    private static Method findFactoryMethod(ConfigurableListableBeanFactory beanFactory, String beanName) {
+        if (beanFactory.containsBeanDefinition(beanName)) {
+            BeanDefinition beanDefinition = beanFactory.getMergedBeanDefinition(beanName);
+            if (beanDefinition instanceof RootBeanDefinition) {
+                RootBeanDefinition rootBeanDefinition = (RootBeanDefinition)beanDefinition;
+                Method resolvedFactoryMethod = rootBeanDefinition.getResolvedFactoryMethod();
+                if (resolvedFactoryMethod != null) {
+                    return resolvedFactoryMethod;
+                }
+            }
+            return findFactoryMethodUsingReflection(beanFactory, beanDefinition);
+        }
+        return null;
+    }
+
+    private static Method findFactoryMethodUsingReflection(ConfigurableListableBeanFactory beanFactory,
+                                                           BeanDefinition beanDefinition) {
+        String factoryMethodName = beanDefinition.getFactoryMethodName();
+        String factoryBeanName = beanDefinition.getFactoryBeanName();
+        if (factoryMethodName == null || factoryBeanName == null) {
+            return null;
+        }
+        Class<?> factoryType = beanFactory.getType(factoryBeanName);
+        if (factoryType.getName().contains(ClassUtils.CGLIB_CLASS_SEPARATOR)) {
+            factoryType = factoryType.getSuperclass();
+        }
+        AtomicReference<Method> factoryMethod = new AtomicReference<>();
+        ReflectionUtils.doWithMethods(factoryType, (method) -> {
+            if (method.getName().equals(factoryMethodName)) {
+                factoryMethod.set(method);
+            }
+        });
+        return factoryMethod.get();
+    }
+
 }
